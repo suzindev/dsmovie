8efe9b6b0cb0a957cf1459e68e42de00b2914af9
suzindev <87452114+suzindev@users.noreply.github.com>
2022-01-15 16:33:23 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -20,15 +20,19 @@ function Listing() {
     });
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}$sort=title`).then(response => {
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`).then(response => {
             const data = response.data as MoviePage;
             setPage(data);
         });
     }, [pageNumber]);
 
+    const handlePageChange = (newPageNumber: number) => {
+        setPageNumber(newPageNumber);
+    }
+
     return (
         <>
-            <Pagination />
+            <Pagination page={page} onChange={handlePageChange} />
             <div className="container">
                 <div className="row">
                     {page.content.map(movie => (
